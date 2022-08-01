381b0aea3509c2c529e70816047ab96b29157016
Viktor Alikin <ludosopher@mail.ru>
2022-08-01 12:01:44 +0300

    Home work 10

---
+++ src/delay/delay.js
@@ -0,0 +1,12 @@
+
+
+
+const delay = (cb, ms) => {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve(cb())
+        }, ms)
+    })
+}
+
+module.exports = delay;
---
+++ src/mapArrToStrings/mapArrToStrings.js
@@ -0,0 +1,9 @@
+
+
+const mapArrToStrings = (arr) => {
+    return arr
+        .filter(item => Number.isInteger(item))
+        .map(String)
+}
+
+module.exports = mapArrToStrings;
